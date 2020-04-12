43528cb3f884d186eeecbd9ac2523c8146f9893c
Eduardo F. J. Heise <eduferreiraj@gmail.com>
2020-04-12 04:29:48 -0300

    [unstable] Float Metric using Decay
    Signed-off-by: Eduardo F. J. Heise <eduferreiraj@gmail.com>

--- flurs/utils/float_metric.py
+++ flurs/utils/float_metric.py
@@ -1,36 +1,34 @@
 class FloatMetric:
-    def __init__(self, w_size):
-        self.w_size = w_size
+    def __init__(self, decay):
+        self.decay = decay
         self.floating_value = 0.0
-        self.counter = 0
+
     def next(self, value):
-        if self.counter < self.w_size:
-            self.counter += 1
-        # old = self.floating_value
         self.update(value)
-        # print("{} [{} -> {}]".format(value, old, self.floating_value))
+
     def get(self):
         return self.floating_value
+
     def __repr__(self):
         return str(self.get())
 
     def update(self, value):
         return
 
-    def is_valid(self):
-        return self.counter == self.w_size
-
 class FloatMean(FloatMetric):
     def update(self, value):
-        self.floating_value = (self.floating_value * (self.counter - 1) + value) / self.counter
+        self.floating_value = self.floating_value * self.decay + value * (1 - self.decay)
 
 class FloatSTD(FloatMetric):
     def __init__(self, mean):
         self.mean = mean
-        super().__init__(self.mean.w_size)
+        super().__init__(self.mean.decay)
+
     def update(self, value):
-        self.floating_value = (self.floating_value * (self.counter - 1) + (value - self.mean.get())**2) / self.counter
+        self.floating_value = (self.floating_value * self.decay + (value - self.mean.get())**2) * (1 - self.decay)
+
     def get(self):
         return self.floating_value**.5
+
     def __repr__(self):
         return "{0:.10f}".format(self.get())
